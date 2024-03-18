309d554ff101c15f90c1be6932c52b1ef0ccdbec
Navjot <navjotcheema289@gmail.com>
2024-03-18 00:31:50 -0500

    Validating html

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
 
   root 'pages#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get 'index', to: 'pages#index'
   get 'about', to: 'pages#about'
-  # Defines the root path route ("/")
-  # root "posts#index"
  
  
   resources :users, only: [:index, :show] do
@@ -20,5 +14,4 @@ Rails.application.routes.draw do
       end
    end
 
-  #resources :users, only: [:index, :show]
 end
